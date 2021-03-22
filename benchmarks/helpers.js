6c7cae930475058e9408ad0ab166d67ac10ae11e
demetrius <tuggledemetrius@gmail.com>
2021-03-21 20:29:30 -0400

    added warmup runs, delay time for CPU and parameterized copies

--- benchmarks/helpers.js
+++ benchmarks/helpers.js
@@ -2,23 +2,23 @@ const cpy = require("cpy");
 const del = require("del");
 const pTimes = require("p-times");
 const exitHook = require("exit-hook");
+const argv = require("minimist");
 
 async function createFiles(subject) {
-  await pTimes(1, async (i) => {
+  const { copies = 10 } = argv(process.argv.slice(2));
+
+  await pTimes(copies, async (i) => {
     await cpy([`./${subject}/${subject}*.js`], `${subject}`, {
       rename: (basename) => `${i + 1}-${basename}`,
     });
   });
-  return subject;
 }
 
 async function deleteFiles(subject) {
   await del([`./${subject}/[0-9]+-${subject}*.js`], { dryRun: false });
 }
 
-exitHook(() => {
-  del.sync(["./**/[0-9]+-*.js"], { dryRun: false });
-});
+exitHook(() => del.sync(["./**/[0-9]+-*.js"], { dryRun: false }));
 
 module.exports = {
   createFiles,
--- benchmarks/index.js
+++ benchmarks/index.js
@@ -1,35 +1,43 @@
 const execa = require("execa");
 const delay = require("delay");
-const { createFiles, deleteFiles } = require("./helpers");
 const pMap = require("p-map");
-const pProps = require("p-props");
+const { createFiles, deleteFiles } = require("./helpers");
+
+process.chdir(__dirname);
 
 const subjects = {
-  ava: "../node_modules/.bin/ava './ava/*.spec.js'",
-  jasmine: "../node_modules/.bin/jasmine ./jasmine/**",
-  jest: "../node_modules/.bin/jest ./jest",
-  mocha: "../node_modules/.bin/mocha ./mocha",
-  pta: "../node_modules/.bin/pta --module-loader cjs ./pta/**",
-  tap: "../node_modules/.bin/tap --no-cov --reporter=classic ./tap",
-  tape: "../node_modules/.bin/tape ./tape/**",
-  uvu: "../node_modules/.bin/uvu ./uvu/",
+  ava: ["--warmup", 10, "../node_modules/.bin/ava './ava/*.spec.js'"],
+  jasmine: ["--warmup", 10, "../node_modules/.bin/jasmine ./jasmine/**"],
+  jest: ["--warmup", 10, "../node_modules/.bin/jest ./jest"],
+  mocha: ["--warmup", 10, "../node_modules/.bin/mocha ./mocha"],
+  pta: [
+    "--warmup",
+    10,
+    "../node_modules/.bin/pta --module-loader cjs ./pta/**",
+  ],
+  tap: [
+    "--warmup",
+    10,
+    "../node_modules/.bin/tap --no-cov --reporter=classic ./tap",
+  ],
+  tape: ["--warmup", 10, "../node_modules/.bin/tape ./tape/**"],
+  uvu: ["--warmup", 10, "../node_modules/.bin/uvu ./uvu/"],
 };
 
 async function benchmarkTestRunners() {
-  process.chdir(__dirname);
   await pMap(Object.keys(subjects), createFiles);
-  await pProps(subjects, benchmark, { concurrency: 1 });
+  await pMap(Object.values(subjects), benchmark, { concurrency: 1 });
   await pMap(Object.keys(subjects), deleteFiles);
 }
 
 async function benchmark(command) {
-  await execa("hyperfine", [command], { stdio: "inherit" });
+  await delay(7000);
+  await execa("hyperfine", command, { stdio: "inherit" });
 }
 
 (async function () {
   await benchmarkTestRunners();
 })().catch((err) => {
-  console.error("Something went wrong!");
   console.error(err);
   process.exit(1);
 });
